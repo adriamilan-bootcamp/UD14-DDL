1f387f586422fd7ca15ed7bbaea545eff9f443f1
Celestinvib <celestinvib@gmail.com>
2022-07-25 19:16:13 +0200

    Ejercicio10 v1.0

---
+++ Ejercicio10/Ejercicio10.sql
@@ -0,0 +1,34 @@
+CREATE DATABASE Ejercicio10;
+
+USE Ejercicio10;
+
+
+CREATE TABLE Cajeros(
+  codigo int NOT NULL AUTO_INCREMENT,
+  nomApels varchar(255),
+  PRIMARY KEY (codigo)
+);
+
+CREATE TABLE Productos(
+  codigo int NOT NULL AUTO_INCREMENT,
+  nombre varchar(100),
+  precio int,
+  PRIMARY KEY (codigo)
+);
+
+CREATE TABLE Maquinas_Registradoras(
+  codigo int NOT NULL AUTO_INCREMENT,
+  piso int,
+  PRIMARY KEY (codigo)
+);
+
+CREATE TABLE Ventas(
+  codigo int NOT NULL AUTO_INCREMENT,
+  cajero int,
+  maquina int,
+  producto int,
+  PRIMARY KEY (codigo),
+  CONSTRAINT codigo_cajero FOREIGN KEY ( cajero ) REFERENCES Cajeros( codigo ),
+  CONSTRAINT codigo_producto FOREIGN KEY ( producto ) REFERENCES Productos( codigo ),
+  CONSTRAINT codigo_maquina_registradora FOREIGN KEY ( maquina ) REFERENCES Maquinas_Registradoras( codigo )
+);

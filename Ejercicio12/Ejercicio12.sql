868d0ae8e56e4d3f5f5eb08c7057c336b5a8b14f
Adrià Milan <adria4milan@gmail.com>
2022-07-29 08:50:22 +0200

    Ejercicio12 Cursos

--- Ejercicio12/Ejercicio12.sql
+++ Ejercicio12/Ejercicio12.sql
@@ -13,5 +13,15 @@ CREATE TABLE Profesores(
   PRIMARY KEY (DNI)
 );
 
-
-
+CREATE TABLE Cursos (
+  cod_curso INT NOT NULL,
+  nombre_curso VARCHAR(45) NULL,
+  dni_profesor VARCHAR(8) NULL,
+  maximo_alumnos INT NULL,
+  fecha_inicio DATE NULL,
+  fecha_final DATE NULL,
+  PRIMARY KEY (cod_curso),
+  CONSTRAINT curso_dni_profesor FOREIGN KEY ( dni_profesor ) REFERENCES Profesores( DNI )
+  ON DELETE SET NULL
+  ON UPDATE CASCADE
+);
